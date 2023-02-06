be3bb6875cccb605f9c381aa9a55d788867b3f0a
Carlos <cmsandoval2@miners.utep.edu>
2023-02-05 17:14:35 -0700

    made paint class

--- myPanel.java
+++ myPanel.java
@@ -4,4 +4,9 @@ public class myPanel extends JPanel{
     myPanel(){
 		this.setPreferredSize(new Dimension(500,500));
 	}
+    public void paint(Graphics g){
+        Graphics2D g2D=(Graphics2D)g;
+        g2D.setStroke(new BasicStroke(2));
+
+    }
 }
